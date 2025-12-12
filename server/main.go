2d07cebd0f64c5237302473d98da61143019c035
ebourmpo <evabourbou@gmail.com>
2025-12-12 20:15:39 +0200

    Fix Go module structure for Render deployment

--- server/main.go
+++ server/main.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"graphQL/handlers"
-	"graphQL/proxy"
+	"graphQL/server/handlers"
+	"graphQL/server/proxy"
 )
 
 func main() {
 	// Serve static files
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
-	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("../scripts"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
 
 	// Routes
 	http.HandleFunc("/", serveTemplate("index.html"))
@@ -30,7 +30,7 @@ func main() {
 // Helper to serve templates with error handling
 func serveTemplate(filename string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmplPath := filepath.Join("../templates", filename)
+		tmplPath := filepath.Join("templates", filename)
 		tmpl, err := template.ParseFiles(tmplPath)
 		if err != nil {
 			http.Error(w, "Failed to load template", http.StatusInternalServerError)
